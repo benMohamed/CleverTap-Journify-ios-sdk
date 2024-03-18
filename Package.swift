85643976e2830f1c6ec2f030918ff2886c71650c
Mohamed Bendnaiba <mohamed.bendnaiba@vosker.com>
2024-03-18 22:16:48 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CleverDestination",
+    name: "CleverTapDestination",
     platforms: [
         .iOS("13.0"),
         .tvOS("11.0"),
@@ -13,8 +13,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CleverDestination",
-            targets: ["CleverDestination"]),
+            name: "CleverTapDestination",
+            targets: ["CleverTapDestination"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -29,7 +29,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CleverDestination",
+            name: "CleverTapDestination",
             dependencies: ["Journify"]),
         
         // TESTS ARE HANDLED VIA THE EXAMPLE APP.
