bf7ff7bcdba64d058a3d3e2e09eaf89bee8cb943
Mohamed Bendnaiba <mohamed.bendnaiba@vosker.com>
2024-03-19 08:59:57 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,12 +18,12 @@ let package = Package(
     ],
     dependencies: [
         .package(
-	    name: "journify-ios-sdk",
+	    name: "Journify",
             url: "https://github.com/journifyio/journify-ios-sdk.git",
             from: "0.0.5"
         ),
 	.package(
-	    name: "clevertap-ios-sdk",
+	    name: "CleverTapSDK",
             url: "https://github.com/CleverTap/clevertap-ios-sdk.git",
             from: "6.1.0"
         )
@@ -33,11 +33,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CleverTapDestination",
-            dependencies: ["journify-ios-sdk", 
-			   .product(
-                                name: "CleverTapSDK",
-                                package: "clevertap-ios-sdk")
-			  ]
+            dependencies: ["Journify", "CleverTapSDK"]
 		),
         
         // TESTS ARE HANDLED VIA THE EXAMPLE APP.
